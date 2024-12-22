fa1bf1a378f213b126293ef33f136a135af1826b
PL <pedrolucasdesousaoliveira@aluno.uespi.br>
2024-12-21 21:13:23 -0300

    Ajustando detalhes no weather_services.dart

--- lib/weather_service.dart
+++ lib/weather_service.dart
@@ -2,7 +2,7 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class WeatherService {
-  final String apiKey = "8aad7eb31eae46718f405111241412"; 
+  final String apiKey = "8aad7eb31eae46718f405111241412";
 
   Future<Map<String, dynamic>> getWeather(String city) async {
     final String url = "https://api.weatherapi.com/v1/forecast.json?key=$apiKey&q=$city&days=1&aqi=no&alerts=no";
@@ -11,9 +11,9 @@ class WeatherService {
       final response = await http.get(Uri.parse(url));
 
       if (response.statusCode == 200) {
-        return json.decode(response.body); 
+        return json.decode(response.body);
       } else {
-        throw Exception("Erro: ${response.statusCode}"); 
+        throw Exception("Erro: ${response.statusCode}");
       }
     } catch (e) {
       throw Exception("Erro na conexão: $e");
